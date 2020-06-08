36c7f21b681ae4053ee95ee55386a8ad16e06a4d
Ama <amangeldiyev.amantur@gmail.com>
2020-06-08 16:24:49 +0500

    user info

--- routes/botman.php
+++ routes/botman.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\BotManController;
 $botman = resolve('botman');
 
 $botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
+    $user = $bot->getUser();
+    $bot->reply(`Hello $user->getFirstName()!`);
 });
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
