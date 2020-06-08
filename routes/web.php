5b522fd33f6ff5841a3908fce061e6ccbca27908
Ama <amangeldiyev.amantur@gmail.com>
2020-06-08 16:26:36 +0500

    user info

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-use BotMan\Drivers\Telegram\TelegramDriver;
+use GuzzleHttp\Client;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,3 +19,15 @@ Route::get('/', function () {
 
 Route::match(['get', 'post'], '/botman', 'BotManController@handle');
 Route::get('/botman/tinker', 'BotManController@tinker');
+
+Route::get('set-webhook', function () {
+    $client = new Client();
+
+    $response = $client->request('POST', 'https://api.telegram.org/bot1110008093:AAExIGim5IkLHV-NCR_HHn7ZYBsFOYSO8RM/setWebhook', [
+        'form_params' => [
+            'url' => 'https://rukzinternational.kz/botman'
+        ]
+    ]);
+
+    dd($response->getBody()->getContents());
+});
