5aae7163347a618e027f5f6c64ff13004d32197b
Ama <amangeldiyev.amantur@gmail.com>
2020-06-09 11:23:46 +0500

    wip

--- routes/botman.php
+++ routes/botman.php
@@ -3,4 +3,4 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
-$botman->hears('cr', BotManController::class.'@startConversation');
+$botman->hears('/cr', BotManController::class.'@startConversation');
