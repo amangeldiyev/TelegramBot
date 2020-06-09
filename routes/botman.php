db84118842c30cde05630e5349174258313faf5d
Ama <amangeldiyev.amantur@gmail.com>
2020-06-09 11:21:01 +0500

    route command

--- routes/botman.php
+++ routes/botman.php
@@ -3,7 +3,4 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
-// $botman->hears('Hi', function ($bot) {
-//     $bot->reply('Hello!');
-// });
-$botman->hears('Hi', BotManController::class.'@startConversation');
+$botman->hears('cr', BotManController::class.'@startConversation');
